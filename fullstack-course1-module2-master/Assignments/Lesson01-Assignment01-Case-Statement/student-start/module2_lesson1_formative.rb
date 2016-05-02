2c923e70b054ca275563e3b339d6ecb486e25716
Rajeev Kaluti <rajeevkaluti@Rajeevs-iMac.local>
2016-05-02 13:24:43 -0400

    Added folder with exercise

---
+++ fullstack-course1-module2-master/Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
@@ -0,0 +1,19 @@
+some_var = false
+another_var = nil
+
+case some_var
+  when "pink elephant"
+    puts "Don't think about the pink elephant!"
+  when false
+    puts "Looks like this one should execute"
+  else
+    puts "I guess nothing matched... But why?"
+end
+
+case another_var
+  when nil
+    puts "Question mark in the method name?"
+  else
+    puts "I quess nothing matched... But why?"
+end  
+
